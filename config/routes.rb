9b3af1977b21656eb9217dc4ff3e3d8831592660
Felipe <felipecorrei@gmail.com>
2013-11-23 23:31:46 -0200

    add db changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 EducationAnalyticsGit01::Application.routes.draw do
+  resources :painels
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131124012615) do
+
+  create_table "painels", force: true do |t|
+    t.string   "nome_app"
+    t.string   "tipo_evento"
+    t.integer  "qtd_evento"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
