7ff5352dc7ee7c4b0c0ffab6ae45a8b1b970babe
Felipe <felipecorrei@gmail.com>
2013-11-23 23:18:06 -0200

    ED_New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EducationAnalyticsGit01::Application.config.secret_key_base = 'cfaef0c0187dc317da186fc752637190630420443d5100a5740db1cfaba1f838a5cd7dacb458cbcc925c61b29e232ab467a389dc29e3677d5901318a74d0e4cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EducationAnalyticsGit01::Application.config.session_store :cookie_store, key: '_EducationAnalytics_git01_session'
